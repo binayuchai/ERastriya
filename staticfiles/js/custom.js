3540457b0f036dd0236fab26b7df574b27ced50c
binayuchai <binayuchai58@gmail.com>
2023-05-08 00:04:46 +0545

    Changes in navigation

--- staticfiles/js/custom.js
+++ staticfiles/js/custom.js
@@ -1,11 +1,19 @@
 /*  Navbar Responsive */
 
-function openNav(){
-    document.getElementById('nav').style.display = "block";
-    document.getElementById('close-btn').style.display = "block";
-}
+// function openNav(){
+//     document.getElementById('nav').style.display = "block";
+//     document.getElementById('close-btn').style.display = "block";
+// }
 
-function closeNav(){
-    document.getElementById('nav').style.display = "none";
-    document.getElementById('close-btn').style.display = "none";
-}
+// function closeNav(){
+//     document.getElementById('nav').style.display = "none";
+//     document.getElementById('close-btn').style.display = "none";
+// }
+
+
+let toggleButton = document.querySelector('.toggle');
+let nav = document.querySelector('.nav');
+
+toggleButton.addEventListener('click', () => {
+  nav.classList.toggle('active');
+});
