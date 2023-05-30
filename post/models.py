a683a3e1eaf0e184b8dc271038a699c46ceeb35b
binayuchai <binayuchai58@gmail.com>
2023-05-30 13:34:08 +0545

    solved issue

--- post/models.py
+++ post/models.py
@@ -88,62 +88,11 @@ class Post(TimeStampModel):
 
     #compress the image 
 
-    # def save(self, *args, **kwargs):
-    #    instance = super(Post, self).save(*args, **kwargs)
-    #    image = Image.open(self.image.path)
-    #    image.save(self.image.path,quality=60,optimize=True)
-    #    return instance
-
     def save(self, *args, **kwargs):
-        super(Post,self).save(*args, **kwargs)
-
-        #Compress CKEditor image
-        self.compress_ckeditor_images()
-
-        #Compress normal image
-        self.compress_normal_image()
-
-
-    def compress_ckeditor_images(self):
-
-        # Finding the image from ckeditor
-        if self.content:
-            # Regular expression to match CKEditor image tags
-            pattern = r'<img[^>]+src="([^">]+)"[^>]*>'
-
-            image_tags = re.findall(pattern,self.content)
-
-            for image_url in image_tags:
-                image_path = self.get_image_path(image_url)
-                if image_path:
-                    self.compress_image(image_path)
-
-
-    def compress_normal_image(self):
-        #Compress the normal image
-        if self.image:
-            image_path = self.image.path
-            self.compress_image(image_path)
-
-    def get_image_path(self,image_url):
-         #Remove the protocol and domain from the image URL
-        # to get the relative path
-        relative_path = image_url.replace(settings.MEDIA_URL, "")
-
-        # Join the relative path with the base media root to get the full file path
-        full_path = os.path.join(settings.MEDIA_ROOT, relative_path)
-        return full_path
-    
-    def compress_image(self,image_path):
-        image = Image.open(image_path)
-        image.save(image_path,quality=60,optimize=True)
-
-        
-
-
-
-            
-
+       instance = super(Post, self).save(*args, **kwargs)
+       image = Image.open(self.image.path)
+       image.save(self.image.path,quality=60,optimize=True)
+       return instance
 
 
 
