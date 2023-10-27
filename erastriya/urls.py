1456e292a70bbeee94c135c2c87b8f1c9b972f57
binayuchai <binayuchai58@gmail.com>
2023-10-27 21:19:16 +0545

    sitemap added

--- erastriya/urls.py
+++ erastriya/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,5 +25,7 @@ urlpatterns = [
     path('ckeditor/',include('ckeditor_uploader.urls')),
     path('',include('home.urls', namespace="home")),
     path('post/',include('post.urls',namespace="post")),
+    path('sitemap.xml/', serve, {'document_root': settings.STATICFILES_DIRS[0], 'path': 'sitemap.xml'}),
+
 
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
