11714c5bffaa08cdaff5a86daf70bfa5e09c5101
binay <binayuchai58@gmail.com>
2024-10-01 19:28:52 +0545

    useradmin added

--- useraccount/admin.py
+++ useraccount/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from useraccount.models import User,Profile
+from django.contrib.auth.admin import UserAdmin
+
 # Register your models here.
-admin.site.register(User)
+admin.site.register(User,UserAdmin)
 admin.site.register(Profile)
